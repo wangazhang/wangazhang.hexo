2a10e8387a486ac7641c8eec3691bf20cada7384
qianzhang <@WSX!QAZ>
2017-12-29 16:48:30 +0800

    add

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+cd /Users/qianzhang/Documents/opt/hexo/wangazhang
+hexo clean
+hexo deploy
+git add .
+git commit -m 'add'
+git push
+cp ~/Documents/opt/hexo/wangazhang/source/_posts/小电开发规范.md /Users/qianzhang/IdeaProjects/rules/README.md
+cd /Users/qianzhang/IdeaProjects/rules
+git add .
+git commit -m 'add'
+git push
